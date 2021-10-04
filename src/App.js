ac49fdac89262fe3f2e1849649a9be3dc50e8ae3
som-syom <dhyeon@student.42seoul.kr>
2021-10-04 14:07:47 +0900

    [WKPL-16] ADD - google login 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Home from "./Home";
+
 function App() {
+  
   return (
     <div className="App">
       Hello World
+      엠키 바보gggggg
+      ggg
+      gggggg
+      <Home />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const CLIENT_ID = '724662600740-hq81l92as2p8b68tcm1e8j7e9gb4herl.apps.googleusercontent.com';
+export const SECRET = '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';
