d449f45a30989f27178e21982ff78a9cc55c6abe
mki <mki@student.42seoul.kr>
2021-10-19 17:18:20 +0900

    [WKPL-34]TEST - nickname in client to server

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
