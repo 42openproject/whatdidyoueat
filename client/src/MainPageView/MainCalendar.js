3aedb3379da77cc8bc9009f772557bd0ee30c916
som-syom <dhyeon@student.42seoul.kr>
2021-11-24 18:05:30 +0900

    FEAT #53 - 더미 데이터 받아와서 달력에 띄우는 기능 추가

--- client/src/MainPageView/MainCalendar.js
+++ client/src/MainPageView/MainCalendar.js
@@ -4,35 +4,67 @@ import Calendar from 'react-calendar';
 import '../stylesheets/MainCalendar.css';
 
 const MainCalendar = ({ clickedDay, setClickedDay }) => {
-  const [date, setDate] = useState(new Date());
+  const [dateVal, setDateVal] = useState(new Date());
+  const [monthVal, setMonthVal] = useState(new Date());
+  const [postedDate, setPostedDate] = useState([]);
 
   const onClickDay = e => {
     console.log(`${e.getMonth() + 1}월 ${e.getDate()}일 입니다`);
     setClickedDay(`${e.getMonth() + 1}월 ${e.getDate()}일 입니다`);
   };
 
+  const setPostedDay = allday => {
+    // console.log(view);
+    if (
+      postedDate.find(
+        d =>
+          d === allday.date.getDate() &&
+          allday.activeStartDate.getMonth() === allday.date.getMonth(),
+      )
+    )
+      return 'calendal--posted-date';
+    return '';
+  };
+
   // test api
   useEffect(async () => {
     const response = await axios.get(`http://localhost:8000/calendar/dhyeon`);
 
-    console.log(response);
+    console.log(response.data.data);
+    setPostedDate(response.data.data);
   }, []);
+
+  const testFormat = () => {
+    return new Intl.DateTimeFormat('ko-KR', {
+      year: 'numeric',
+      month: 'numeric',
+    }).format(monthVal);
+  };
+
+  const onActiveStartDateChange = data => {
+    console.log(data);
+    const oldDate = dateVal;
+    if (data.action !== 'onChange') setMonthVal(data.activeStartDate);
+  };
 
   return (
     <>
       <Calendar
         // className="main-calendar"
-        onChange={setDate}
-        value={date}
+        onChange={setDateVal}
+        value={dateVal}
         maxDate={new Date()}
         locale="en-US"
         onClickDay={onClickDay}
-        formatMonthYear={() =>
-          new Intl.DateTimeFormat('ko-KR', {
-            year: 'numeric',
-            month: 'numeric',
-          }).format(date)
-        }
+        // formatMonthYear={() =>
+        //   new Intl.DateTimeFormat('ko-KR', {
+        //     year: 'numeric',
+        //     month: 'numeric',
+        //   }).format(dateVal)
+        // }
+        formatMonthYear={testFormat}
+        tileClassName={setPostedDay}
+        onActiveStartDateChange={onActiveStartDateChange}
       />
     </>
   );
