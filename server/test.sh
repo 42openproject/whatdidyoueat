9f9b0b4b8c655d43f6ec7fa69f6b2763fb6e0faf
mki <mki@student.42seoul.kr>
2021-11-30 09:09:03 +0900

    FIX - test.sh 추가

--- server/test.sh
+++ server/test.sh
@@ -1,14 +1,24 @@
+
+# POST /login/google
+curl \
+-H "Content-Type: application/json" \
+-d '{"googleId":"123", "email":"mki@student.42seoul.kr"}' \
+-X POST 'http://localhost:3001/login/google'
+
+# POST /users/nickname
+curl \
+-H "Content-Type: application/json" \
+-d '{"nickname":"mki", "googleId":"123"}' \
+-X POST 'http://localhost:3001/users/nickname'
+
 # GET /users/nickname?googleId=
 # curl -X GET 'http://localhost:3001/users/nickname?googleId=104760257817400625505'
 
-# POST /users/nickname
-# curl \
-# -H "Content-Type: application/json" \
-# -d '{"nickname":"456", "googleId":"456"}' \
-# -X POST 'http://localhost:3001/users/nickname'
+# GET /title/:id
+# curl -X GET 'http://localhost:3001/titles/456'
 
-# POST /login/google
-# curl \
-# -H "Content-Type: application/json" \
-# -d '{"googleId":"456", "email":"starswot@gmail.com"}' \
-# -X POST 'http://localhost:3001/login/google'
+# POST /title/mki
+curl \
+-H "Content-Type: application/json" \
+-d '{"googleId":"123", "title":"mki의 이유식"}' \
+-X POST 'http://localhost:3001/titles/mki'
