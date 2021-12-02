13533bbef4e56009d10d1b3ebeb51b67c33aa588
mki <mki@student.42seoul.kr>
2021-12-03 05:43:02 +0900

    FIX - test.sh 수정 및 주석 추가

--- server/test.sh
+++ server/test.sh
@@ -1,10 +1,20 @@
-
+# -------------------------------------------------------------------
+# -----------------------/login/google-------------------------------
+# -------------------------------------------------------------------
 # POST /login/google
+curl \
+-H "Content-Type: application/json" \
+-d '{"googleId":"1234", "email":"mki@student.42seoul.kr"}' \
+-X POST 'http://localhost:3001/login/google'
+
 # curl \
 # -H "Content-Type: application/json" \
-# -d '{"googleId":"123", "email":"mki@student.42seoul.kr"}' \
+# -d '{"googleId":"123"}' \
 # -X POST 'http://localhost:3001/login/google'
 
+# -------------------------------------------------------------------
+# -----------------------/users/nickname-----------------------------
+# -------------------------------------------------------------------
 # POST /users/nickname
 # curl \
 # -H "Content-Type: application/json" \
@@ -14,16 +24,19 @@
 # GET /users/nickname?googleId=
 # curl -X GET 'http://localhost:3001/users/nickname?googleId=104760257817400625505'
 
+# -------------------------------------------------------------------
+# -----------------------/title/:id----------------------------------
+# -------------------------------------------------------------------
 # GET /title/:id
 # curl -X GET 'http://localhost:3001/titles/123'
 
 # POST /title/:id
 # body: googleId, title
-curl \
--H "Content-Type: application/json" \
--d '{"googleId":"123", "title":"mki의 이유식"}' \
--X POST 'http://localhost:3001/titles/mki'
+# curl \
+# -H "Content-Type: application/json" \
+# -d '{"googleId":"123", "title":"mki의 이유식"}' \
+# -X POST 'http://localhost:3001/titles/mki'
 
 # GET /title/:id
 # query parameter: date=YYYY-MM-DD
-curl -X GET 'http://localhost:3001/titles/mki?date=2021-12-01'
+# curl -X GET 'http://localhost:3001/titles/mki?date=2021-12-01'
