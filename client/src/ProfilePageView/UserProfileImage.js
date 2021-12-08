ed421c925d47cbef9cb1aaff24ad35d1d83060da
som-syom <dhyeon@student.42seoul.kr>
2021-12-08 14:02:19 +0900

    FEAT #40 - S3에 업로드하고 location 반환 성공

--- client/src/ProfilePageView/UserProfileImage.js
+++ client/src/ProfilePageView/UserProfileImage.js
@@ -1,26 +1,53 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { MdAddPhotoAlternate } from 'react-icons/md';
 
 function UserProfileImage() {
-  const [userImage, setUserImage] = useState('');
-  const defaultUserImage =
-    'https://karateinthewoodlands.com/wp-content/uploads/2017/09/default-user-image-300x300.png';
+  const [userImage, setUserImage] = useState(
+    'https://karateinthewoodlands.com/wp-content/uploads/2017/09/default-user-image-300x300.png',
+  );
 
-  useEffect(async () => {
-    // user image 요청 추가
+  const setFile = e => {
+    console.log('setFile click');
+    if (e.target.files[0]) {
+      const img = new FormData();
+      img.append('file', e.target.files[0]);
+      axios.post(`http://localhost:3001/upload/image`, img).then(res => {
+        console.log(res);
 
-    // test api
-    const { data } = await axios.get(`http://localhost:8000/profileImg/dhyeon`);
-    // console.log(data);
-    setUserImage(data.imgUrl);
-  }, [userImage]);
+        setUserImage(res.data);
+      });
+    }
+  };
+
+  // useEffect(async () => {
+  //   // user image 요청 추가
+
+  //   // test api
+  //   // const { data } = await axios.get(`http://localhost:8000/profileImg/dhyeon`);
+  //   // console.log(data);
+  //   // setUserImage(data.imgUrl);
+  // }, [userImage]);
 
   return (
     <>
-      <div className="user-profile__img btn">
-        <img
-          src={userImage === '' ? defaultUserImage : userImage}
-          className="user-profile__img__img"
+      <div className="user-profile__img">
+        <img src={userImage} className="user-profile__img__img" />
+      </div>
+      <div className="user-profile__img--overlay">
+        <label
+          for="profile-img-upload"
+          className="user-profile__img__upload btn"
+        >
+          <MdAddPhotoAlternate />
+        </label>
+        <input
+          id="profile-img-upload"
+          name="img"
+          type="file"
+          accept="image/*"
+          onChange={setFile}
+          style={{ display: 'none' }}
         />
       </div>
     </>
--- server/routes/img.js
+++ server/routes/img.js
@@ -1,8 +1,11 @@
 var express = require("express");
-const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const imgService = require("../services/img");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+aws.config.loadFromPath(__dirname + "/../config/s3.json");
 var router = express.Router();
 
 /**
@@ -30,25 +33,37 @@ var router = express.Router();
 //   }
 // });
 
+const s3 = new aws.S3();
 const upload = multer({
-  // dest: "uploads/",
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3: s3,
+    bucket: "images.whatdidyoueat",
+    acl: "public-read-write",
+    key: function (req, file, cb) {
+      cb(null, Date.now() + "." + file.originalname.split(".").pop()); // 이름 설정
     },
   }),
-  limits: { fileSize: 5 * 1024 * 1024 },
 });
+
+// const upload = multer({
+//   // dest: "uploads/",
+//   storage: multer.diskStorage({
+//     destination(req, file, cb) {
+//       cb(null, "uploads/");
+//     },
+//     filename(req, file, cb) {
+//       const ext = path.extname(file.originalname);
+//       cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//     },
+//   }),
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// });
 
 router.post("/image", upload.single("file"), function (req, res, next) {
   // console.log(req);
   // console.log(req);
   console.log(req.file);
-  res.status(200).send(req.file);
+  res.status(200).send(req.file.location);
   // imgService.uploadImg(req, res, next);
 });
 
