fc27bbee45d091ace5d75f4ff31256e589a14027
som-syom <dhyeon@student.42seoul.kr>
2021-12-08 10:26:03 +0900

    ADD #94 - 컴포넌트 분리, 내용 작성

---
+++ client/src/SearchPageView/SearchPost.js
@@ -0,0 +1,11 @@
+function SearchPost() {
+  return (
+    <>
+      <section className="all-post-box">
+        <div className="post">포스트</div>
+      </section>
+    </>
+  );
+}
+
+export default SearchPost;
